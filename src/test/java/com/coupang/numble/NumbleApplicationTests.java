1024adcc011850064d1645f28fa8fc6245b5e396
kms <kmslkh@naver.com>
2021-11-18 01:15:49 +0900

    Spring Boot Test Profile 설정

--- src/test/java/com/coupang/numble/NumbleApplicationTests.java
+++ src/test/java/com/coupang/numble/NumbleApplicationTests.java
@@ -3,7 +3,7 @@ package com.coupang.numble;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active:prod")
 class NumbleApplicationTests {
 
     @Test
