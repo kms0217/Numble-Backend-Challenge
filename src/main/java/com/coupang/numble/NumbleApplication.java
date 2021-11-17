b044736d0ecaf0ce0a8ec642b9bc03840fb4962a
kms <kmslkh@naver.com>
2021-11-17 21:53:05 +0900

    init

---
+++ src/main/java/com/coupang/numble/NumbleApplication.java
@@ -0,0 +1,13 @@
+package com.coupang.numble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumbleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coupang/numble/NumbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupang.numble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
